e96c1d4b9c910069dd42c0abda2fa8465276cba7
MendozaJulioC <ing.juliomendoza@icloud.com>
2022-09-20 16:46:09 -0500

    update index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,8 @@ const router = Router();
 
 const piCtrl = require('../controllers/index.controllers')
 
-
 router
     .get('/',piCtrl.getHome)
     .get('/api/pi/dependencias', piCtrl.getDependencias)
-
 
 module.exports = router;
